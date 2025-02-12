068e5e3ad19e07c44d7656570d60e2ebe12ca0ef
WhiteSunOfSpace <nikromodin@mail.ru>
2025-02-12 21:12:44 +0300

    Add Delete function

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ dp = Dispatcher()
 
 user_TODO = {}
 
-valid_options = ["Introduction", "Github link", "Use TODO", "Help", "Show tasks", "/start"]
+valid_options = ["Introduction", "Github link", "Use TODO", "Help", "/start"]
 
 keyboard = ReplyKeyboardMarkup(
     keyboard=[
@@ -35,6 +35,9 @@ todo_keyboard = ReplyKeyboardMarkup(
 class TodoState(StatesGroup):
     wait_for_input = State()
 
+class DeleteState(StatesGroup):
+    wait_for_input = State()
+
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     await message.answer("Hello, this bot is ready to use:", reply_markup=keyboard)
@@ -49,7 +52,7 @@ async def cmd_hello(message: types.Message, state: FSMContext):
     uid = message.from_user.id
     if uid in user_TODO and user_TODO[uid]:
         todo_list = "\n".join([f"{num}) {task}" for num, task in enumerate(user_TODO[uid], start=1)])
-        await message.answer(f"Your TODO is:\n{todo_list}", reply_markup=keyboard)
+        await message.answer(f"Your TODO is:\n{todo_list}", reply_markup=todo_keyboard)
     else:
         await message.answer('Your TODO is empty', reply_markup=keyboard)
     await state.clear()
@@ -62,6 +65,23 @@ async def get_link(message: types.Message, state: FSMContext):
 @dp.message(F.text.contains('Help'))
 async def cmd_help(message: types.Message, state: FSMContext):
     await message.answer('If you need help, contact us via whitesunofspace@mail.ru')
+    await state.clear()
+
+@dp.message(F.text.contains('Delete task'))
+async def cmd_delete(message: types.Message, state: FSMContext):
+    await message.answer('Put the number of task which need to delete')
+    await state.set_state(DeleteState.wait_for_input)
+
+@dp.message(DeleteState.wait_for_input)
+async def process_todo_delete(message: types.Message, state: FSMContext):
+    num = int(message.text)
+    user_id = message.from_user.id
+    temp = []
+    for i in range(len(user_TODO[user_id])):
+        if i != num-1:
+            temp.append(user_TODO[user_id][i])
+    user_TODO[user_id] = temp
+    await message.answer("Task is delete", reply_markup=todo_keyboard)
     await state.clear()
 
 async def cmd_act(message: types.Message, state: FSMContext):
